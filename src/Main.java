d12d510f81c98b2790c30e118a6ccef3c4a52da8
Alperen Aktuğ <aktugalperen9@gmail.com>
2025-03-18 23:01:47 +0300

    installitation

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main (String[] args){
+
+        // print()
+
+        System.out.println("Merhaba dünya");
+        System.out.println("I like hamburger");
+        System.out.println("the very interesting!!");
+
+        // single line comments()
+
+        // This is first my java program
+
+        // multi line comments ()
+
+        /*
+           Look
+            if you had
+             one shot one oppurtinuty
+        */
+
+    }
+}
